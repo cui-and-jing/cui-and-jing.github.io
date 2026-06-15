9a8fa7feb4ae882cebac5d5ed7413172738224af
cui-and-jing <cui-and-jing@users.noreply.github.com>
2026-06-15 03:02:28 +0000

    deploy: 92a06d066ce6957f0958eb49f81b9729a4c94a3b

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/06/14/first-post/","2026/06/14/github-actions-deploy/","2026/06/14/hello-world/","2026/06/14/hexo-deploy-guide/"];function toRandomPost(){
+var posts=["2026/06/15/github-pages-guide/","2026/06/15/hello-world/","2026/06/15/markdown-guide/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
